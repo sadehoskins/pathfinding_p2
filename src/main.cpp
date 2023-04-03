b89c8a9a10c23a6b81bd4c116e825e001ca49108
Oliver Pruchnicki <35111378+SquidSushi@users.noreply.github.com>
2023-04-03 23:30:28 +0200

    The feature is here! (only in width but that is like, OK)

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 int main() {
     // Raylib initialization
     // Project name, screen size, fullscreen mode etc. can be specified in the config.h.in file
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
     InitWindow(Game::ScreenWidth, Game::ScreenHeight, Game::PROJECT_NAME);
     SetTargetFPS(60);
 
@@ -18,6 +19,9 @@ int main() {
     // ...
     // ...
     Texture2D myTexture = LoadTexture("assets/graphics/testimage.png");
+    RenderTexture2D canvas = LoadRenderTexture(Game::ScreenWidth, Game::ScreenHeight);
+    float renderScale{};
+    Rectangle renderRec{};
 
     // Main game loop
     while (!WindowShouldClose()) // Detect window close button or ESC key
@@ -27,13 +31,21 @@ int main() {
         // ...
 
         BeginDrawing();
-            // You can draw on the screen between BeginDrawing() and EndDrawing()
-            // ...
-            // ...
-            ClearBackground(WHITE);
-            DrawText("Hello, world!", 10, 10, 30, LIGHTGRAY);
-            DrawTexture(myTexture, 10, 100, WHITE);
-
+        BeginTextureMode(canvas);
+        // You can draw on the screen between BeginDrawing() and EndDrawing()
+        // ...
+        // ...
+        ClearBackground(WHITE);
+        DrawText("Hello, world!", 10, 10, 30, LIGHTGRAY);
+        DrawTexture(myTexture, 10, 100, WHITE);
+        EndTextureMode();
+        ClearBackground(BLACK);
+        renderScale = GetScreenHeight() / (float)canvas.texture.height;
+        renderRec.width = canvas.texture.width * renderScale;
+        renderRec.height = canvas.texture.height * renderScale;
+        renderRec.x = GetScreenWidth()/2 - renderRec.width/2;
+        DrawTexturePro(canvas.texture, Rectangle{0, 0, (float) canvas.texture.width, (float) -canvas.texture.height}, renderRec,
+                       {}, 0, WHITE);
         EndDrawing();
     } // Main game loop end
 
