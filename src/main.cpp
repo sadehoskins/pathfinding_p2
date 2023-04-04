e1fd6842ba1e99a7cdcac6d4f9b3570bdab5fcad
oliver.pruchnicki <35111378+SquidSushi@users.noreply.github.com>
2023-04-04 10:45:55 +0200

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -43,12 +43,13 @@ int main() {
         EndTextureMode();
         //The following lines put the canvas in the middle of the window and have the negative as black
         ClearBackground(BLACK);
-        renderScale = std::min(GetScreenHeight() / (float) canvas.texture.height, //Calculates how big or small the canvas has to be rendered.
+        renderScale = std::min(GetScreenHeight() /
+                               (float) canvas.texture.height, //Calculates how big or small the canvas has to be rendered.
                                GetScreenWidth() / (float) canvas.texture.width);
         renderRec.width = canvas.texture.width * renderScale;
         renderRec.height = canvas.texture.height * renderScale;
-        renderRec.x = (GetScreenWidth() - renderRec.width) / 2.0f ;
-        renderRec.y = (GetScreenHeight()- renderRec.height) / 2.0f;
+        renderRec.x = (GetScreenWidth() - renderRec.width) / 2.0f;
+        renderRec.y = (GetScreenHeight() - renderRec.height) / 2.0f;
         DrawTexturePro(canvas.texture, Rectangle{0, 0, (float) canvas.texture.width, (float) -canvas.texture.height},
                        renderRec,
                        {}, 0, WHITE);
