964fa4e3f003cd6d0515688624126630c70cf8dc
oliver.pruchnicki <35111378+SquidSushi@users.noreply.github.com>
2024-04-10 14:20:11 +0200

    Added flooring to renderscale
    also some formatting and comments

--- src/main.cpp
+++ src/main.cpp
@@ -52,15 +52,19 @@ int main() {
         }
         EndTextureMode();
         //The following lines put the canvas in the middle of the window and have the negative as black
-        ClearBackground(BLACK);
-        renderScale = std::min(GetScreenHeight() /
-                               (float) canvas.texture.height, //Calculates how big or small the canvas has to be rendered.
-                               GetScreenWidth() / (float) canvas.texture.width);
+        ClearBackground(BLACK); // If you want something else than a black void in the background
+                                // then you can add stuff here.
+
+        renderScale = std::min(GetScreenHeight() / (float) canvas.texture.height, // Calculates how big or small the canvas has to be rendered.
+                               GetScreenWidth()  / (float) canvas.texture.width); // Priority is given to the smaller side.
+        renderScale = floorf(renderScale);
+        if (renderScale < 1) renderScale = 1; // Ensure that scale is at least 1.
         renderRec.width = canvas.texture.width * renderScale;
         renderRec.height = canvas.texture.height * renderScale;
         renderRec.x = (GetScreenWidth() - renderRec.width) / 2.0f;
         renderRec.y = (GetScreenHeight() - renderRec.height) / 2.0f;
-        DrawTexturePro(canvas.texture, Rectangle{0, 0, (float) canvas.texture.width, (float) -canvas.texture.height},
+        DrawTexturePro(canvas.texture,
+                       Rectangle{0, 0, (float) canvas.texture.width, (float) -canvas.texture.height},
                        renderRec,
                        {}, 0, WHITE);
         EndDrawing();
